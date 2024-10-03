e8211e9feb0ad8fc24c5a46f38b96ea9ddd69e2c
ppwlsw <pinpawat.l@ku.th>
2024-10-03 14:02:57 +0700

    Make Faci more flexible

--- src/app/components/faci_card.jsx
+++ src/app/components/faci_card.jsx
@@ -1,9 +1,9 @@
-export default function FacilityCard({ icons, topic, detail }) {
+export default function FacilityCard(props) {
     return (
         <div className="relative z-0 flex flex-col items-center justify-around bg-[#E4DFD9] h-[27rem] w-[19rem] py-[3.5rem] px-[1.5rem] transition-transform duration-300 ease-in-out hover:scale-105">
-            <div className="text-[3.75rem]">{icons}</div>
-            <div className="text-[2.25rem]">{topic}</div>
-            <div className="text-wrap text-center text-[1.5rem]">{detail}</div>
+            <div className="text-[3.75rem]">{props.icons}</div>
+            <div className="text-[2.25rem]">{props.topic}</div>
+            <div className="text-wrap text-center text-[1.5rem]">{props.detail}</div>
         </div>
     );
 }
