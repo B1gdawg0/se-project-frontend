7eef392a91348e2186a2ed8184d2045354ee3b70
tent <ithikorn.u@ku.th>
2024-11-04 21:08:42 +0700

    add reservation slip payment

--- src/app/components/choose-zone.jsx
+++ src/app/components/choose-zone.jsx
@@ -4,45 +4,54 @@ import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import beer from "../../images/beer.png";
 import { useRouter } from 'next/navigation';
-import {GetMenu} from "../../hook/menu"
+import { GetMenu } from "../../hook/menu"
 import { CheckUserToken } from '../../../utils/token';
 import { BookTable } from '../../hook/table';
 import { GetUser } from '../../hook/user';
 import { GetUserData } from '../../../utils/user';
 
-function ChooseZone({zones}) {
+function ChooseZone({ zones }) {
     const [isButtonVisible, setButtonVisible] = useState(true);
     const [isDropdownVisible, setDropdownVisible] = useState(false);
     const [isConfirmCardVisible, setConfirmCardVisible] = useState(0);
     const [zone, setZone] = useState(null);
     const [selectedItem, setSelectedItem] = useState(null);
     const [discountApplied, setDiscountApplied] = useState(false);
+    const [file, setFile] = useState(null);
+    const [receiptUrl, setReceiptUrl] = useState(null);
     const [menu, setMenu] = useState(null)
+    const [isClickPurchase, setIsClickPurchase] = useState(false);
     const router = useRouter();
 
-    const createFirstOrder = async ()=>{
-        try{
+    const createFirstOrder = async () => {
+        try {
             const user = GetUserData();
             const data = {
-                payload:{
-                    "c_id":user.ID,
+                payload: {
+                    "c_id": user.ID,
                     "t_status": "R",
+                    "o_url": receiptUrl,
                     "orderline": {
                         "m_id": selectedItem['m_id'],
                         "quantity": "1",
                         "price": `${selectedItem['m_price']}`,
-                        }
+                    }
                 },
-                TID:zone
+                TID: zone
             }
-    
+
             const res = await BookTable(data)
-            if(res.status === 200){
+            if (res.status === 200) {
+                setIsClickPurchase(false)
                 return setConfirmCardVisible(3)
             }
-        }catch(err){
+        } catch (err) {
             alert(err)
         }
+    }
+
+    const handlePurchase = () => {
+        setIsClickPurchase(true);
     }
 
     const handleApplyDiscount = () => {
@@ -57,20 +66,29 @@ function ChooseZone({zones}) {
         setDropdownVisible(!isDropdownVisible);
     };
 
-    useEffect(()=>{
-        
-        const fetchMenu = async()=>{
+    const handleFileChange = (e) => {
+        const file = e.target.files?.[0];
+        if (file) {
+          setFile(file);
+          setReceiptUrl(URL.createObjectURL(file));
+        }
+        console.log("Receipt URL : ", receiptUrl);
+      };
+
+    useEffect(() => {
+
+        const fetchMenu = async () => {
             const res = await GetMenu()
 
-            if(res.status === 200){
+            if (res.status === 200) {
                 setMenu(res.data.payload.menus)
             }
         }
 
-        if(CheckUserToken()){
+        if (CheckUserToken()) {
             fetchMenu();
         }
-    },[])
+    }, [])
 
     const handleZoneClick = (zone) => {
         if (zones[zone]['t_status'] === 'A') {
@@ -92,9 +110,9 @@ function ChooseZone({zones}) {
     return (
         <div className="flex flex-col gap-2">
             {
-                isButtonVisible && 
-                <div 
-                    onClick={handleToggleDropdown} 
+                isButtonVisible &&
+                <div
+                    onClick={handleToggleDropdown}
                     className="border-2 border-white rounded-md px-6 py-1 cursor-pointer">
                     <p className="text-white text-4xl font-bold text-center flex items-center">
                         Choose your zone here
@@ -105,9 +123,9 @@ function ChooseZone({zones}) {
 
             {isDropdownVisible && (
                 <div className="bg-gray-300 bg-opacity-30 rounded-lg transition-opacity duration-300">
-                    {Object.entries(zones).map(([zoneKey,obj]) => (
-                        <div 
-                            key={zoneKey} 
+                    {Object.entries(zones).map(([zoneKey, obj]) => (
+                        <div
+                            key={zoneKey}
                             onClick={() => handleZoneClick(zoneKey)}
                             className="p-4 text-white transition-transform duration-300 hover:scale-105 hover:border-2 hover:border-white rounded-md text-3xl cursor-pointer">
                             <div className='flex justify-between'>
@@ -132,19 +150,19 @@ function ChooseZone({zones}) {
                     <p className='text-4xl'>Choose your favorite open set</p>
                     <div className='flex flex-col gap-2 w-full'>
                         {Object.entries(menu).map(([index, obj]) => (
-                            <div 
-                                key={index} 
+                            <div
+                                key={index}
                                 className={`flex items-center gap-2 p-2 border border-gray-300 rounded-lg shadow-md transition duration-300 
                                     ${selectedItem && selectedItem['m_description'] === obj['m_description'] ? 'bg-gray-200' : 'bg-white'} 
-                                    ${selectedItem && selectedItem['m_description'] !== obj['m_description']  ? 'opacity-50' : ''}`} 
-                                onClick={() => handleSelect(obj)} 
+                                    ${selectedItem && selectedItem['m_description'] !== obj['m_description'] ? 'opacity-50' : ''}`}
+                                onClick={() => handleSelect(obj)}
                             >
                                 <div className='relative flex-shrink-0 w-[30%] h-24'>
-                                <img 
-                                    src={obj['m_url']} 
-                                    alt={obj['m_description']} 
-                                    className='absolute top-0 left-0 w-full h-full object-cover' 
-                                />
+                                    <img
+                                        src={obj['m_url']}
+                                        alt={obj['m_description']}
+                                        className='absolute top-0 left-0 w-full h-full object-cover'
+                                    />
                                 </div>
                                 <div className='flex flex-col justify-center flex-grow p-2'>
                                     {/* <p className='font-semibold text-3xl'>{obj.name}</p> */}
@@ -155,7 +173,7 @@ function ChooseZone({zones}) {
                         ))}
                     </div>
 
-                    {selectedItem && 
+                    {selectedItem &&
                         <button className='px-8 py-3 text-xl bg-black text-white font-bold rounded-lg' onClick={() => setConfirmCardVisible(2)}>
                             Next
                         </button>}
@@ -163,30 +181,30 @@ function ChooseZone({zones}) {
             )}
 
             {isConfirmCardVisible === 2 && (
-            <div className='w-full bg-white py-5 px-8 rounded-xl flex flex-col gap-5 items-center'>
-                <div className="flex flex-col gap-3 mx-10">
-                    <p className="text-black text-4xl font-bold">Discount</p>
-                    <div className="flex">
-                        <input type="text" placeholder="abc-def-ghi" className="border-2 border-black rounded-l-md px-4 py-1 text-4xl"></input>
-                        <button className='px-4 py-1 text-4xl rounded-r-lg bg-black text-white' onClick={handleApplyDiscount} >Apply</button>
+                <div className='w-full bg-white py-5 px-8 rounded-xl flex flex-col gap-5 items-center'>
+                    <div className="flex flex-col gap-3 mx-10">
+                        <p className="text-black text-4xl font-bold">Discount</p>
+                        <div className="flex">
+                            <input type="text" placeholder="abc-def-ghi" className="border-2 border-black rounded-l-md px-4 py-1 text-4xl"></input>
+                            <button className='px-4 py-1 text-4xl rounded-r-lg bg-black text-white' onClick={handleApplyDiscount} >Apply</button>
+                        </div>
                     </div>
-                </div>
-                <p className='text-3xl font-semibold'>Apply Discount Code</p>
-                <p className={`text-2xl font-semibold flex items-center ${discountApplied ? 'text-zinc-400 line-through' : 'text-black'}`}>
-                    Regular Price: 
-                    <span className="ml-2">{`$${selectedItem['m_price'].toFixed(2)}`}</span>
-                </p>
-
-                {discountApplied && (
-                    <p className='text-4xl font-bold text-green-600'>
-                        Discounted Price: ${selectedItem ? (selectedItem['m_price'] - 5).toFixed(2) : ''}
+                    <p className='text-3xl font-semibold'>Apply Discount Code</p>
+                    <p className={`text-2xl font-semibold flex items-center ${discountApplied ? 'text-zinc-400 line-through' : 'text-black'}`}>
+                        Regular Price:
+                        <span className="ml-2">{`$${selectedItem['m_price'].toFixed(2)}`}</span>
                     </p>
-                )}
 
-                <button className='px-8 py-3 text-xl bg-black text-white font-bold rounded-lg' onClick={createFirstOrder}>
-                    Pay
-                </button>
-            </div>
+                    {discountApplied && (
+                        <p className='text-4xl font-bold text-green-600'>
+                            Discounted Price: ${selectedItem ? (selectedItem['m_price'] - 5).toFixed(2) : ''}
+                        </p>
+                    )}
+
+                    <button className='px-8 py-3 text-xl bg-black text-white font-bold rounded-lg' onClick={handlePurchase}>
+                        Pay
+                    </button>
+                </div>
             )}
 
             {isConfirmCardVisible === 3 && (
@@ -200,6 +218,44 @@ function ChooseZone({zones}) {
                             className="px-8 py-3 mt-4 bg-green-700 text-white font-bold rounded-lg text-xl"
                         >
                             Continue
+                        </button>
+                    </div>
+                </div>
+            )}
+
+            {isClickPurchase && (
+                <div className="fixed inset-0 bg-background bg-opacity-90 flex items-center justify-center z-50 p-4 sm:p-8">
+                    <div className="bg-background p-6 border border-main rounded-lg shadow-lg">
+                        <div className="flex justify-center mb-4">
+                            <Image
+                                src="https://www.scb.co.th/content/media/personal-banking/digital-banking/scb-easy/how-to/qr-code/qr-code-generated-7.jpg"
+                                width={250}
+                                height={250}
+                                alt="QR-Code"
+                            />
+                        </div>
+                        <div className="flex flex-col mb-4">
+                            <label
+                                className="mb-2 text-sm font-medium text-gray-900"
+                                htmlFor="file_input"
+                            >
+                                Upload file
+                            </label>
+                            <input
+                                id="file_input"
+                                type="file"
+                                onChange={handleFileChange}
+                                className="text-black border border-gray-300 rounded-lg bg-gray-50 p-2"
+                            />
+                            <div className="text-white mt-2">
+                                {file ? file.name : "No file selected"}
+                            </div>
+                        </div>
+                        <button
+                            onClick={createFirstOrder}
+                            className="bg-main text-black rounded-xl w-full py-2 text-lg font-bold my-2 hover:bg-opacity-80 transition duration-300"
+                        >
+                            Confirm Purchase
                         </button>
                     </div>
                 </div>
